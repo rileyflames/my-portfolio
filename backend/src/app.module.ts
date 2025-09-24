fdcd7649b93d6cab9f796fc6ad6bf8828f4eee69
rileyflamez <Rileyflamez@proton.me>
2025-09-24 16:41:17 +0200

    finished configuring the project to work with 2 Databases and to use GraphQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,65 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
+// GraphQL imports
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+// DataBase Imports
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+
+// App Parts will go here
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // load the .ev
+    ConfigModule.forRoot({
+      isGlobal : true
+    }),
+
+    // GraphQL
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver : ApolloDriver,
+      autoSchemaFile : join(process.cwd(), 'src/schema.gql'),
+      playground : true
+    }),
+
+    // PostgreSQL TypeORM
+    TypeOrmModule.forRootAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      // important
+      name: process.env.POSTGRES_CONNECTION_NAME, // NEEDED for working with multiple DBs
+      useFactory : (configService : ConfigService) => ({
+        type: 'postgres',
+        host : configService.get<string>('POSTGRES_HOST'),
+        port : parseInt(configService.get<string>('POSTGRES_PORT') ?? '5432'),
+        username : configService.get<string>('POSTGRES_USER'),
+        password : configService.get<string>('POSTGRES_PASSWORD'),
+        database : configService.get<string>('POSTGRES_DB'),
+
+        // ENTITIES
+
+        entities : [__dirname + '/../**/*.entity{.ts,.}'],
+
+        synchronize : true // in dev : false in prod
+      }),
+    }),
+
+    // MongoDB (Mongoose)
+    MongooseModule.forRootAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+
+    // all my other modules
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
